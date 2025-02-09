4eb42b891f1f2c5f90df1bb74be8f44dc62caf2e
sbotchey23 <sbotchey23@hotmail.com>
2025-02-09 08:39:38 +0000

    updated small screen css elements

--- ckefaMedia2/ckefa.js
+++ ckefaMedia2/ckefa.js
@@ -1,6 +1,25 @@
-$('.menu-sm').click(function () {
-  $(this).toggleClass('active');
-  console.log("Clicked menu");
-  $(".nav-link-list").toggleClass("show-list");
-  $(".nav-link-list").fadeIn();
+$(document).ready(function () {
+  $(".close-sm").hide();
+  $(".hamburger-sm").click(function () {
+    $(".nav-link-list").slideToggle("slow", function () {
+      $(".hamburger-sm").hide();
+      $(".close-sm").show();
+    });
+  });
+
+  $(".close-sm").click(function () {
+    $(".nav-link-list").slideToggle("slow", function () {
+      $(".close-sm").hide();
+      $(".hamburger-sm").show();
+    });
+  });
+
 });
+
+// $('.menu-sm').click(function () {
+//   $(this).toggleClass('active');
+//   console.log("Clicked menu");
+//   $(".nav-link-list").toggleClass("show-list");
+//   $(".nav-link-list").fadeIn();
+// });
+
